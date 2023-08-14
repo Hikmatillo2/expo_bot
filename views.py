e6a4246853ea5d7546c9c3ed3775d6f46eb255d9
hikmatillo <dolgovdaniil007@gmail.com>
2023-08-14 21:37:54 +0700

    fixes

--- views.py
+++ views.py
@@ -18,25 +18,4 @@ def get_message(request: django.http.HttpRequest):
     return HttpResponse('Method Not Allowed', 405)
 
 
-@csrf_exempt
-def new_issue(request: django.http.HttpRequest):
-    data = json.loads(request.body.decode('utf-8'))
-    if assignee_alert(data):
-        return HttpResponse('!', 200)
-    return HttpResponse('Error', 404)
-
-
-@csrf_exempt
-def issue_state(request: django.http.HttpRequest):
-    data = json.loads(request.body.decode('utf-8'))
-    if notify(data):
-        return HttpResponse('!', 200)
-    return HttpResponse('Error', 404)
-
-
-@csrf_exempt
-def new_comment(request: django.http.HttpRequest):
-    pass
-
-
 # bot.set_webhook(url=f'bot.hikmatillo.ru/{settings.BOT_TOKEN}')
